a03fb76ac3872234cec37f57179cd8d66a0dcf1f
Hubert Łępicki <hubert.lepicki@amberbit.com>
2018-06-19 13:20:20 +0200

    simple version inserting 1 record at 1 visit

--- api/lib/api/controllers/page_controller.ex
+++ api/lib/api/controllers/page_controller.ex
@@ -2,6 +2,9 @@ defmodule Api.PageController do
   use Api.Web, :controller
 
   def index(conn, _params) do
+
+    #Db.Repo.insert_all("visits", [%{remote_ip: inspect(conn.remote_ip), host: conn.host, request_path: conn.request_path}])
+
     conn
     |> render("index.json")
   end
